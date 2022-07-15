e9a94738170828fca3edd4ada9e19334f08c3b08
wanghong <qq3158424005@gmail.com>
2022-07-15 17:15:03 +0800

    somethingSimpleButDegraded 2.1

--- dayDayUp/itstack-demo-design/itstack-pureProxyPattern/src/main/java/org/itstack/demo/design/agent/MapperFactoryBean.java
+++ dayDayUp/itstack-demo-design/itstack-pureProxyPattern/src/main/java/org/itstack/demo/design/agent/MapperFactoryBean.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class MapperFactoryBean<T> implements FactoryBean<T> {
