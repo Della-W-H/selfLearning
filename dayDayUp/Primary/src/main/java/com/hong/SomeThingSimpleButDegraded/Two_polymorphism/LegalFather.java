621c668f93d3bb91fa6a2a92c2af9551ea4eb6b9
wanghong <qq3158424005@gmail.com>
2022-07-22 10:40:29 +0800

    somethingSimpleButDegraded 2.2

---
+++ dayDayUp/Primary/src/main/java/com/hong/SomeThingSimpleButDegraded/Two_polymorphism/LegalFather.java
@@ -0,0 +1,9 @@
+package com.hong.SomeThingSimpleButDegraded.Two_polymorphism;
+
+/**
+ * @author wanghong
+ * @date 2022/7/22
+ * @apiNote 抽象类 可以不必须实现 接口所有的默认方法 不实现一个都行
+ */
+public abstract class LegalFather implements Father{
+}
--- dayDayUp/itstack-demo-design/itstack-observePattern/src/main/java/org/itstack/demo/design/change/event/listener/EventListener.java
+++ dayDayUp/itstack-demo-design/itstack-observePattern/src/main/java/org/itstack/demo/design/change/event/listener/EventListener.java
@@ -1,6 +1,5 @@
 package org.itstack.demo.design.change.event.listener;
 
-
 import org.itstack.demo.design.change.event.LotteryResult;
 
 public interface EventListener {
